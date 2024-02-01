d7b11531c69f7e213304983a07c72ce4d5b52cd3
kennedy-ak <akogokennedy@gmail.com>
2024-02-01 01:07:57 +0000

    building the catalog view and creating the templates

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -16,6 +17,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+    
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category,related_name='products', on_delete=models.CASCADE)
@@ -29,10 +34,14 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
 
+    
+
     class Meta:
         ordering = ('name',)
         index_together = (('id','slug'),)
     
     def __str__(self):
         return self.name
-    
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id,self.slug])   
