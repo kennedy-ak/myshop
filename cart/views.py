eaaffbd05c30f5db1415044dd8fc2dd05a377253
kennedy-ak <akogokennedy@gmail.com>
2024-02-02 15:03:59 +0000

    working on the cart

--- cart/views.py
+++ cart/views.py
@@ -35,3 +35,11 @@ def cart_detail(request):
     })
 
 
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={
+                            'quantity': item['quantity'],
+                            'override': True})
+    return render(request, 'cart/detail.html', {'cart': cart})
+
