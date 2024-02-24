bb5c68bfc22ccc0e5eece3a946ce289f629ff384
kennedy-ak <akogokennedy@gmail.com>
2024-02-24 00:29:38 +0000

    adding pagination

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from cart.forms import CartAddProductForm
 # Create your views here.
@@ -16,10 +17,23 @@ def product_list (request,category_slug=None):
         products = products.filter(category=category)
 
 
+    #adding pagination to the category page
+    paginator = Paginator.page(categories,7)
+    page = request.GET.get('page')
+    try:
+        cate = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer
+        cate = paginator.page(1)
+    except EmptyPage:
+        cate = paginator.page(paginator.num_pages)
+
+
     return render( request, 'shop/product/list.html', 
     {'category':category,
     'categories':categories,
-    'products':products})
+    'products':products,
+    'cate':cate})
 
 def product_detail(request,id,slug):
     product = get_object_or_404(Product,id = id, slug=slug,available=True)
